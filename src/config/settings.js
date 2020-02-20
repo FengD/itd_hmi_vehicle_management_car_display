f6822285a1d27b0610382818fe3872bee75faf1a
feng.ding <feng.ding1@hirain.com>
2020-02-20 22:03:29 +0800

    <fix>(route): change server route to itdipdhmi

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 // url
 // export const serverIp = '10.10.51.152:9999';
-export const serverIp = 'dingfeng.website:9999';
+// export const serverIp = 'dingfeng.website:9999';
+export const serverIp = 'itdipdhmi.hirain.com:9999';
 // export const serverIp = 'localhost:9999';
 // export const serverIp = '192.168.8.135:9999';
-
